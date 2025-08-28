596685d1eb520a6ff31c3e00311feb3d360e7e53
tirednyxx <95273860+tirednyxx@users.noreply.github.com>
2025-08-29 01:49:01 +0800

    Update 1rectangle.php

--- 1rectangle.php
+++ 1rectangle.php
@@ -1,1 +1,29 @@
+<?php
+// name of the class will be Rectangle
+class Rectangle {
+    // default values for width and height is 1
+    private $width = 1;
+    private $height = 1;
 
+    // Constructor to create a rectangle with the specified width and height
+    public function __construct($width = 1, $height = 1) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    // method to return area of the rectangle
+    public function getArea() {
+        return $this->width * $this->height;
+    }
+
+    // method to return perimeter of the rectangle
+    public function getPerimeter() {
+        return 2 * ($this->width + $this->height);
+    }
+}
+
+// Sample object
+$rect = new Rectangle(6, 4);
+echo "Area: " . $rect->getArea() . "\n";         // Output should be: Area: 24
+echo "Perimeter: " . $rect->getPerimeter();      // Output should be: Perimeter: 20
+?>
